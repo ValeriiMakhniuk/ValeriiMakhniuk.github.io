cf151a5b1b2fe80a00a3137e126e633aae10b6f1
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2019-08-27 14:17:13 +0300

    add animation via JS to team__accordeon

---
+++ js/accordeon.js
@@ -0,0 +1,13 @@
+const accordeon_item = document.getElementsByClassName('team__accordeon-item');
+
+for (let i = 0; i < accordeon_item.length; i++) {
+  accordeon_item[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    if (accordeon_item[i].classList.contains('team__accordeon-item-active')) {
+      accordeon_item[i].classList.remove('team__accordeon-item-active');
+    }
+    else {
+      accordeon_item[i].classList.add('team__accordeon-item-active');
+    }
+  });
+}
